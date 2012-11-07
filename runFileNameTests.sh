a5809b507d27278d8ec60401ec26ab3d36210638
Stu <stu@stu-macbook-ii.home>
2012-11-07 13:33:16 +0000

    File Clean Up
    Found out about .gitignore :)

---
+++ runFileNameTests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+xx=`pwd`
+IFS=\
+
+cd /Volumes/App\ Data/Development/DTPO\ Autoload\ Test/Sample\ Files/
+for file in *
+do
+	if [ "$1" == "load" ];
+	then
+		python $xx/dtpoautoload.py --config_file /Volumes/App\ Data/Development/DTPO\ Autoload\ Test/etc/dtpo_autoload.conf $file
+	else
+		python $xx/dtpoautoload.py --test_parse  --config_file /Volumes/App\ Data/Development/DTPO\ Autoload\ Test/etc/dtpo_autoload.conf $file
+	fi
+done
